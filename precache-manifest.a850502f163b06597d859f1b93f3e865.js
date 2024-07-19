0a06176ee6f6cf672f8890b9ddffa829f7f85083
juv123 <deegaaug@gmail.com>
2024-07-20 00:40:16 +0530

    Updates

---
+++ precache-manifest.a850502f163b06597d859f1b93f3e865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a585e9c0938f2bb7a7a2d99d16ee761c",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "34feb989721a1a2c4ca9",
+    "url": "/shoppingcart/static/css/main.55902797.chunk.css"
+  },
+  {
+    "revision": "249da71eb0a586ed476b",
+    "url": "/shoppingcart/static/js/2.a876df18.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/shoppingcart/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "34feb989721a1a2c4ca9",
+    "url": "/shoppingcart/static/js/main.a148c42e.chunk.js"
+  },
+  {
+    "revision": "25d5c9a86bfe905f2598",
+    "url": "/shoppingcart/static/js/runtime~main.78c680eb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/shoppingcart/static/media/logo.06e73328.svg"
+  }
+]);
